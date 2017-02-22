a8861d3041ff7c6ce694b6b3b1e6431d31499fb5
Colin Turner <colinturner92@gmail.com>
2017-02-22 10:31:11 +0700

    Ex 2

---
+++ ex2.rb
@@ -0,0 +1,12 @@
+# These hashes, or octothorpes, create comments that are ignored
+# by the program
+
+=begin
+Here is a multi-
+line comment
+This is almost like poetry	
+=end
+
+puts "No comments printed!"
+
+#And take one final comment just because
